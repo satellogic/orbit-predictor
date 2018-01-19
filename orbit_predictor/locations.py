e0a1d2dbf12e3d24aa1c9fde9f8bef5e5f2a477d
Diego Mascialino <diego.mascialino@satellogic.com>
2018-01-19 15:10:21 -0300

    Allow to load locations from custom_locations.py

--- orbit_predictor/locations.py
+++ orbit_predictor/locations.py
@@ -251,11 +251,8 @@ india = Location('india', latitude_deg=23.5, longitude_deg=78.5, elevation_m=550
 moscu = Location('moscu', latitude_deg=55.7, longitude_deg=37.5, elevation_m=137)
 niger = Location('niger', latitude_deg=20, longitude_deg=12.5, elevation_m=430)
 riogrande = Location("RIOGRANDE", latitude_deg=-53.8, longitude_deg=-67.75, elevation_m=30)
-svalbard = Location("SVALBARD", latitude_deg=78.229772, longitude_deg=15.407786, elevation_m=501)
-# HAM
-DK3WN = Location("DK3WN", latitude_deg=49.7317, longitude_deg=8.9548, elevation_m=138)
-JA6PL = Location("JA6PL", latitude_deg=33.8685, longitude_deg=130.719, elevation_m=10)
-LU4EOU = Location("LU4EOU", latitude_deg=-38.7243008, longitude_deg=-62.2449124, elevation_m=10)
-# tortu1 == tortu2 == LU1CGB
-tortu1 = Location("TORTU1", latitude_deg=-34.4791, longitude_deg=-58.7916, elevation_m=10)
-tortu2 = Location("TORTU2", latitude_deg=-34.4791, longitude_deg=-58.7916, elevation_m=10)
+
+try:
+    from orbit_predictor.custom_locations import *
+except ModuleNotFoundError:
+    pass
