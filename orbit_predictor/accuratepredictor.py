9298c137595c93f6d1e61f82a1813217a1d5d454
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2019-04-08 15:29:46 +0200

    Flake8

--- orbit_predictor/accuratepredictor.py
+++ orbit_predictor/accuratepredictor.py
@@ -1,8 +1,12 @@
 # For backwards compatibility
 import warnings
+
+from .predictors.accurate import HighAccuracyTLEPredictor, ONE_SECOND
+
 warnings.warn(
     "Use `from orbit_predictor.predictors import HighAccuracyTLEPredictor` instead",
     FutureWarning,
 )
 
-from .predictors.accurate import HighAccuracyTLEPredictor, ONE_SECOND
+
+__all__ = ["HighAccuracyTLEPredictor", "ONE_SECOND"]
