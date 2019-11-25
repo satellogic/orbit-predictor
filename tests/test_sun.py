848ab749087e497afb890298c7f05182e1c9b35b
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-11-26 00:21:51 +0100

    Add tests for get_shadow

--- tests/test_sun.py
+++ tests/test_sun.py
@@ -3,7 +3,7 @@ import datetime as dt
 import numpy as np
 import pytest
 
-from orbit_predictor.utils import angle_between, get_sun
+from orbit_predictor.utils import angle_between, get_sun, get_shadow
 
 
 # Data obtained from Astropy using the JPL ephemerides
@@ -19,3 +19,29 @@ def test_get_sun_matches_expected_result_within_precision(when_utc, expected_eci
 
     assert angle_between(eci, expected_eci) < 1.0  # Claimed precision
     assert angle_between(eci, expected_eci) < 0.5  # Actual precision
+
+
+# Data obtained from GMAT
+@pytest.mark.parametrize("when_utc,r_ecef,expected_shadow", [
+    [dt.datetime(2000, 1, 1, 12, 9, 0), np.array([1272.929355, 6984.992047, 1299.821897]), 2],
+    [dt.datetime(2000, 1, 1, 12, 30, 0), np.array([-7298.548961, 500.322464, 639.443822]), 0],
+])
+def test_get_shadow_matches_expected_result(when_utc, r_ecef, expected_shadow):
+    shadow = get_shadow(r_ecef, when_utc)
+
+    assert shadow == expected_shadow
+
+
+# Data obtained from GMAT
+# Testing the penumbra is much harder, because it only lasts a few seconds
+# and the uncertainty in the Sun position is even larger than the angle difference
+# between umbra and penumbra
+@pytest.mark.xfail
+@pytest.mark.parametrize("when_utc,r_ecef", [
+    [dt.datetime(2000, 1, 1, 12, 10, 5), np.array([-2779.471958, 6565.365892, 1625.185914])],
+    [dt.datetime(2000, 1, 1, 12, 10, 15), np.array([-2842.327184, 6539.439097, 1625.522584])],
+])
+def test_get_shadow_gives_penumbra(when_utc, r_ecef):
+    shadow = get_shadow(r_ecef, when_utc)
+
+    assert shadow == 1
