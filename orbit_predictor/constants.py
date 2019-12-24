759cfeda35d9d29a632dcc661e67784d7e34888b
Juan Luis Cano Rodríguez <juanlu@satellogic.com>
2019-12-24 14:41:08 +0100

    Add Earth flattening
    Fix #77.

--- orbit_predictor/constants.py
+++ orbit_predictor/constants.py
@@ -8,5 +8,6 @@ AU = 149597870.700  # km
 OMEGA = 2 * pi / (86400 * 365.2421897)  # rad / s
 MU_E = wgs84.mu  # km3 / s2
 R_E_KM = wgs84.radiusearthkm  # km
+F_E = 1 / 298.257223560
 J2 = wgs84.j2
 OMEGA_E = 7.292115e-5  # rad / s
