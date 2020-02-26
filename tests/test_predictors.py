f6bbf69ab376990390c8ef525b4b6ae85eb5d5f8
Juan Luis Cano Rodríguez <juanlu@satellogic.com>
2020-02-27 00:16:06 +0100

    Add tests for get_normal_vector

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -1,9 +1,10 @@
 import datetime as dt
 
 from hypothesis import given
-from hypothesis.strategies import datetimes
+from hypothesis.strategies import datetimes, composite, floats
 import pytest
 
+from orbit_predictor.constants import R_E_KM
 from orbit_predictor.predictors.keplerian import KeplerianPredictor
 from orbit_predictor.predictors.numerical import J2Predictor
 from orbit_predictor.utils import eclipse_duration
@@ -21,6 +22,28 @@ def non_sun_synchronous():
     return J2Predictor(
         7000, 0, 42, 0, 0, 0, dt.datetime(2020, 2, 26, 0, 0, 0)
     )
+
+
+@composite
+def equatorial_orbits(
+    draw,
+    sma=floats(R_E_KM, 42000),
+    ecc=floats(0, 1, exclude_max=True),
+    argp=floats(0, 360),
+    ta=floats(0, 360),
+    epoch=datetimes(),
+):
+    return KeplerianPredictor(
+        draw(sma), draw(ecc), 0, 0, draw(argp), draw(ta), draw(epoch),
+    )
+
+
+@given(predictor=equatorial_orbits(), when_utc=datetimes())
+def test_get_normal_vector_zero_inclination_always_z_aligned(predictor, when_utc):
+    normal_vector = predictor.get_normal_vector(when_utc)
+
+    assert normal_vector[0] == normal_vector[1] == 0
+    assert normal_vector[2] == 1
 
 
 @given(datetimes())
