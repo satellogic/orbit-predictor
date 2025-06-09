df81b9756d668170506a4b10e55afdcefda7e059
Matias Graña <matias@satellogic.com>
2025-06-09 17:26:25 -0300

    fix non_sun_synchronous as function

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -25,7 +25,6 @@ def elliptical_orbit():
     )
 
 
-@pytest.fixture()
 def non_sun_synchronous():
     return J2Predictor(
         7000, 0, 42, 0, 0, 0, dt.datetime(2020, 2, 26, 0, 0, 0)
@@ -71,8 +70,9 @@ def test_get_normal_vector_zero_inclination_always_z_aligned(predictor, when_utc
 
 
 @given(datetimes())
-def test_get_beta_always_between_m_90_and_90(non_sun_synchronous, when_utc):
-    assert -90 <= non_sun_synchronous.get_beta(when_utc) <= 90
+def test_get_beta_always_between_m_90_and_90(when_utc):
+    nss = non_sun_synchronous()
+    assert -90 <= nss.get_beta(when_utc) <= 90
 
 
 def test_get_eclipse_duration_fails_for_eccentric_orbits(elliptical_orbit):
@@ -82,14 +82,15 @@ def test_get_eclipse_duration_fails_for_eccentric_orbits(elliptical_orbit):
     assert "Non circular orbits are not supported" in excinfo.exconly()
 
 
-def test_get_eclipse_duration_changes_for_non_sun_synchronous_satellite(non_sun_synchronous):
+def test_get_eclipse_duration_changes_for_non_sun_synchronous_satellite():
+    nss = non_sun_synchronous()
     dt1 = dt.datetime(2020, 2, 1, 0, 0, 0)
     dt2 = dt.datetime(2020, 2, 15, 0, 0, 0)
 
-    maximum_eclipse_duration = eclipse_duration(0, non_sun_synchronous.period)
+    maximum_eclipse_duration = eclipse_duration(0, nss.period)
 
-    eclipse_duration1 = non_sun_synchronous.get_eclipse_duration(dt1)
-    eclipse_duration2 = non_sun_synchronous.get_eclipse_duration(dt2)
+    eclipse_duration1 = nss.get_eclipse_duration(dt1)
+    eclipse_duration2 = nss.get_eclipse_duration(dt2)
 
     assert eclipse_duration1 <= maximum_eclipse_duration
     assert eclipse_duration2 <= maximum_eclipse_duration
